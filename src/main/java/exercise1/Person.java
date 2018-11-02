8a42e6a6dae6e71f13a6b6f7d2238ce0ee9c7a75
Marylise27 <172393@student.umons.ac.be>
2018-11-02 15:00:53 +0100

    Exercice 1

--- src/main/java/exercise1/Person.java
+++ src/main/java/exercise1/Person.java
@@ -1,4 +1,25 @@
 package exercise1;
 
 class Person {
+	private int age;
+	private String name;
+
+	public Person(){
+		age = 0;
+		name = null;
+	}
+
+	public Person(String name,int age){
+		this.age = age;
+		this.name = name;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public String getName() {
+		return name;
+	}
+
 }
