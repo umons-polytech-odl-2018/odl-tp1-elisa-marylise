97460b059a7ec1d01be175960f5e930a86533d61
Marylise27 <172393@student.umons.ac.be>
2018-11-02 15:23:53 +0100

    Exercice 2

--- src/main/java/exercise2/Person.java
+++ src/main/java/exercise2/Person.java
@@ -1,4 +1,26 @@
 package exercise2;
 
 class Person {
+	private int age;
+	private static int cmpt = 0;
+	private static int ageTotal = 0;
+
+	Person(int age) {
+		this.age=age;
+		cmpt++;
+		ageTotal+=age;
+	}
+
+	static int computePopulationSize() {
+		return cmpt;
+	}
+
+	static float computeAveragePopulationAge() {
+		return (float)ageTotal/(float)cmpt;
+	}
+
+	static void resetPopulation() {
+		cmpt = 0;
+		ageTotal = 0;
+	}
 }
